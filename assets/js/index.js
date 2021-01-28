a0472e378bcc99cff7aebdd3be107a4165eed1df
Karl Heitmann <khf1988@gmail.com>
2021-01-28 20:51:47 -0300

    fibonacci_word added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+function fibonacci_word(n) {
+  if (n == 0) {
+    return '0'
+  } else if (n == 1) {
+    return '01'
+  } else {
+    return fibonacci_word(n - 1) + fibonacci_word(n - 2)
+  }
+}
+
+const fibonacci_string = fibonacci_word(10);
+console.log(fibonacci_string);
+
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.moveTo(0, 0);
