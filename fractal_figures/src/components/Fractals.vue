b0e0347251272762cbb5e43e8dbf45d143c8aae0
Karl Heitmann <khf1988@gmail.com>
2021-01-29 14:03:28 -0300

    encapsulada parte q no funcionaba en una funcion

--- fractal_figures/src/components/Fractals.vue
+++ fractal_figures/src/components/Fractals.vue
@@ -16,6 +16,76 @@
 </template>
 
 <script>
+function fibonacci_word(n) {
+  if (n == 0) {
+    return '0'
+  } else if (n == 1) {
+    return '01'
+  } else {
+    return fibonacci_word(n - 1) + fibonacci_word(n - 2)
+  }
+}
+
+const fibonacci_string = fibonacci_word(15);
+console.log(fibonacci_string);
+
+const inteligencia = {
+  'w': {
+    0: 'n',
+    1: 's'
+  },
+  'n': {
+    0: 'e',
+    1: 'w'
+  },
+  'e': {
+    0: 's',
+    1: 'n'
+  },
+  's': {
+    0: 'w',
+    1: 'e'
+  },
+}
+
+console.log(inteligencia);
+function run() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+
+  let direction = 'w';
+  let i = 0;
+  const STEP = 1;
+  let x = 50;
+  let y = 85;
+  let x_step, y_step;
+
+  fibonacci_string.split('').forEach(paso => {
+    x_step = x
+    y_step = y
+    if (direction == 'w') {
+      x_step += STEP;
+    } else if (direction == 'e') {
+      x_step -= STEP;
+    } else if (direction == 'n') {
+      y_step += STEP;
+    } else if (direction == 's') {
+      y_step -= STEP;
+    }
+    ctx.moveTo(x, y);
+    ctx.lineTo(x_step, y_step);
+    ctx.stroke();
+    if (paso == '0') {
+      direction = inteligencia[direction][i % 2]
+    }
+    x = x_step
+    y = y_step
+    i +=1
+    // # puts i
+  });
+}
+console.log(run);
+
 export default {
   name: 'HelloWorld',
   props: {
