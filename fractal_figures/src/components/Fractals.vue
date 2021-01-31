4b56757e3655ceacc2ea3e74fda1c3f682aee241
Karl Heitmann <khf1988@gmail.com>
2021-01-31 15:19:18 -0300

    button auto on sidebar works :D

--- fractal_figures/src/components/Fractals.vue
+++ fractal_figures/src/components/Fractals.vue
@@ -103,42 +103,37 @@ export default {
       console.log("sidebarMessageReceived", arg1);
       if (arg1 == 'auto') {
         console.log('auto');
+        if (this.running) {
+          clearInterval(this.fractalsIntervalId);
+          this.running = false;
+          this.start_text = 'Start';
+          this.fibonacci_string = this.brush.fibonacci_string;
+        } else {
+          this.brush = newBrush(this.fibonacci_n, {x: Number(this.origin_x), y: Number(this.origin_y)}, this.stroke_size);
+          let canvas = document.getElementById('myCanvas');
+          canvas.width = 0;
+          canvas.width = 2000;
+          this.vueCanvas.beginPath();
+          this.fibonacci_string = this.brush.fibonacci_string;
+          this.fractalsIntervalId = setInterval(()=> {
+            step(this.vueCanvas, this.brush)
+            // console.log(this.brush.fibonacci_string.length)
+            // console.log(this.brush.i)
+            if (this.brush.fibonacci_string.length < this.brush.i) {
+              this.start_text = 'Start';
+              this.running = false;
+              clearInterval(this.fractalsIntervalId);
+            }
+          }, 25)
+          this.running = true;
+          this.start_text = 'STOP';
+        }
       } else if (arg1 == 'manual') {
         console.log('manual');
       }
     },
     childMessageReceived: function(arg1) {
       console.log("WOW", arg1);
-    },
-    step: function () {
-      // const fibonacci_string = fibonacciWord(Number(this.message));
-      // this.message = this.message.split('').reverse().join('')
-      // step(this.vueCanvas, this.brush)
-      if (this.running) {
-        clearInterval(this.fractalsIntervalId);
-        this.running = false;
-        this.start_text = 'Start';
-        this.fibonacci_string = this.brush.fibonacci_string;
-      } else {
-        this.brush = newBrush(this.fibonacci_n, {x: Number(this.origin_x), y: Number(this.origin_y)}, this.stroke_size);
-        let canvas = document.getElementById('myCanvas');
-        canvas.width = 0;
-        canvas.width = 2000;
-        this.vueCanvas.beginPath();
-        this.fibonacci_string = this.brush.fibonacci_string;
-        this.fractalsIntervalId = setInterval(()=> {
-          step(this.vueCanvas, this.brush)
-          // console.log(this.brush.fibonacci_string.length)
-          // console.log(this.brush.i)
-          if (this.brush.fibonacci_string.length < this.brush.i) {
-            this.start_text = 'Start';
-            this.running = false;
-            clearInterval(this.fractalsIntervalId);
-          }
-        }, 25)
-        this.running = true;
-        this.start_text = 'STOP';
-      }
     },
     reset: function () {
       this.brush = newBrush(this.fibonacci_n, {x: Number(this.origin_x), y: Number(this.origin_y)}, Number(this.stroke_size));
