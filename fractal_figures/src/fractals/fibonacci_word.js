5b3a40b5f05f853d46b2e335343f14b3ecaa3854
Karl Heitmann <khf1988@gmail.com>
2021-01-31 17:48:23 -0300

    corrected bug on Brush class

--- fractal_figures/src/fractals/fibonacci_word.js
+++ fractal_figures/src/fractals/fibonacci_word.js
@@ -61,13 +61,19 @@ class Brush {
 
   calculateStroke() {
     if (this.direction == 'w') {
-      this.x_next += this.step;
+      console.log(this.x)
+      console.log(this.x_next)
+      this.x_next += this.stroke_size;
+      console.log("HOLA")
+      console.log(this.x)
+      console.log(this.x_next)
+
     } else if (this.direction == 'e') {
-      this.x_next -= this.step;
+      this.x_next -= this.stroke_size;
     } else if (this.direction == 'n') {
-      this.y_next += this.step;
+      this.y_next += this.stroke_size;
     } else if (this.direction == 's') {
-      this.y_next -= this.step;
+      this.y_next -= this.stroke_size;
     }
   }
 
